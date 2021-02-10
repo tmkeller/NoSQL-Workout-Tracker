4ebcb748c67a6b9e0781c23a26bc4aa5834a4601
Tim Keller <timothy.m.keller@gmail.com>
2021-02-09 19:30:29 -0800

    Styling for workout header and delete workout button. Added submit add new click event. Added get routes for individual workouts and exercises, and post routes for workouts and exercises. Added proper hbsObj for home get route. Got submit new workout modal working and entered code for workout header and delete button.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,8 +2,17 @@ const db = require("../models");
 
 module.exports = function (app) {
 
-    app.get("/", (req, res) => {
-        res.render( "index" );
+    app.get("/", async (req, res) => {
+
+        const exercises = await db.Exercise.find({}).lean()
+        .then( dbExercise => {
+            return dbExercise;
+        })
+        const hbsObj = {
+            exercises
+        }
+        console.log( hbsObj )
+        res.render( "index", hbsObj );
     });
 
 }
